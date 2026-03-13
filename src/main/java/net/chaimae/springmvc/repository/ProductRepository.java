084a0b2930069c6d015671ee7d9a25b43330d1d2
Chaimae Mouhssine <chaimaemouhssine60@gmail.com>
2026-03-13 21:17:28 +0000

    springmvcapplication

--- src/main/java/net/chaimae/springmvc/repository/ProductRepository.java
+++ src/main/java/net/chaimae/springmvc/repository/ProductRepository.java
@@ -3,5 +3,6 @@ package net.chaimae.springmvc.repository;
 import net.chaimae.springmvc.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ProductRepository  extends JpaRepository<Product, Long> {
 }
